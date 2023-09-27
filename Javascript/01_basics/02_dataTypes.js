df0d89f0e862bc6a30d45d46d5940cdf63292935
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-09-27 00:03:45 +0000

    dataTypes

---
+++ Javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3+ 3)     --------------> we are using nodejs, not browser 
+
+console.log( 3+3)
+// code readability should be high
+console.log("code readability")
+
+
+// data types
+
+let name ="niraj"
+let age=21
+let isLoggedIn = false
+
+//range of number => 2 to power 53
+//bigint 
+//string => ""
+// boolean=> true/false
+//null => standalone value----empty
+let state = null; 
+//undefined => not initialized
+//symbol => to find uniqueness
+
+//---------------------
+
+//object
+
+console.log(typeof "niraj")   //string
+console.log(typeof age)       //number
+console.log(typeof isLoggedIn)   //boolean
+console.log(typeof undefined)     //undefined
+console.log(typeof null)        //object
