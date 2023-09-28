b9b49dfcbbb98cda3e9085033991c66f7b3476a5
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-09-28 15:07:12 +0000

    04

---
+++ Javascript/01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+
+//comparison between different data type
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);  //false
+console.log(null == 0);   //false
+
+console.log(null>=0);  //true
+console.log(null<=0);  //true
+
+
+console.log(undefined == 0);   //false
+console.log(undefined >= 0);   //false
+console.log(undefined <= 0);   //false
+
+
+console.log("2"===2);  //strict check to check data type and its value
