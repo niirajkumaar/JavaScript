6c0d2e3eca560bf360676b97512ee39ea5cd672a
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-09-29 12:03:42 +0000

    Math

--- Javascript/01_basics/07_nums_and_maths.js
+++ Javascript/01_basics/07_nums_and_maths.js
@@ -20,3 +20,26 @@ console.log(otherNumber.toPrecision(1))   //2e+1
 const hundreds=1000000
 console.log(hundreds.toLocaleString())   //american standard
 console.log(hundreds.toLocaleString('en-IN'))   //indian standard
+
+// -----------------MATHS---------------->>>>>>>>>>>>>>>
+
+console.log(Math)
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+
+console.log(Math.floor(4.2))
+
+console.log(Math.min(4,9,2,6))
+console.log(Math.max(4,9,2,6))
+
+console.log(Math.random())  //always in bw 0 and 1
+
+console.log((Math.random()*10)+1)  // 1 to 9
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()* (max-min+1))+min)
+
+
