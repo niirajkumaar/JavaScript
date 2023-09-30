b54614a2851928f6bba5177e99d00e24d73fe16e
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-09-30 01:45:53 +0000

    date and time

--- Javascript/01_basics/08_dates.js
+++ Javascript/01_basics/08_dates.js
@@ -13,7 +13,7 @@ console.log(typeof myDate)
 
 
 let myCreatedDate = new Date(2023, 0, 23)
-// 0 means january  if we write in single digit ...but if we write double digit then it is as it was
+// 0 means january  if we write with comma ...but if we write in double quotation then it is as it was
 let doubleMonth = new Date("2023-01-25")
 console.log(doubleMonth)
 console.log(myCreatedDate.toDateString())
@@ -21,3 +21,18 @@ console.log(myCreatedDate.toDateString())
 
  myCreatedDate = new Date(2023, 0, 23, 5, 3)
  console.log(myCreatedDate.toLocaleString())
+
+
+ let myTimeStamp = Date.now()
+ console.log(myTimeStamp)
+ console.log(myCreatedDate.getTime())
+ console.log(Math.floor(Date.now()/1000))
+
+
+ let newDate = new Date()
+ console.log(newDate)
+
+ newDate.toLocaleString('default',{
+    weekday:"long",
+    
+ })
