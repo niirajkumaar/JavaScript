813a0e00cd4bdb997723691c7fd08b39b254c5ce
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-10-10 05:00:37 +0000

    scopes

---
+++ Javascript/03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+//var c=300
+let a =300
+if(true){
+    let a = 10
+    const b=20
+    
+
+}
+
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+function one(){
+    const username = "niraj"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+one()
