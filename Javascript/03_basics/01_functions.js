fe9adad5c4fec795575e3754c7c2e51b572cd01f
Niraj Kumar <75536156+niirajkumaar@users.noreply.github.com>
2023-10-10 04:40:52 +0000

    function completed

--- Javascript/03_basics/01_functions.js
+++ Javascript/03_basics/01_functions.js
@@ -29,3 +29,25 @@ function loginUser(username){
     return `${username} just logged in`
 }
 console.log(loginUser())
+
+//...Rest operator 
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,600))
+
+const user = {
+    username : "niraj",
+    price : 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
+
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
